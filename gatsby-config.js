f9b94887e69865ad4b317890c58284d6ba604ca6
Rahul Sawant <rahulsawanth06@gmail.com>
2019-07-05 19:01:35 +0530

    added required deps with husky setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Lanyard`,
+    description: `The Agenda PWA for your Conference`,
+    author: `StackTinkers`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -13,16 +13,22 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `GOOGLE_ANALYTICS_TRACKING_ID`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Lanyard`,
+        short_name: `Lanyard`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#FF9900`,
+        theme_color: `#FF9900`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
