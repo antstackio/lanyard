a60a0da82c5f207c47184e8ada70ab22e9d5b9f5
Praveen G <praveenasvenom@gmail.com>
2019-07-24 00:43:25 +0530

    404 ui updated

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,37 @@
 import React from "react"
-
-import Layout from "../components/layout"
+import {navigate} from "gatsby"
+import four_o_four from "../images/404.svg"
+import {button} from "../components/jss/cvcss"
 import SEO from "../components/seo"
+import { css } from "@emotion/core"
 
 const NotFoundPage = () => (
-  <Layout>
+  <div css={fourError}>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+    <img src={four_o_four} alt="404"/>
+    <div className="text">
+    <h1 style={{fontSize: "8vw"}}>Oops...! something went wrong.</h1>
+    <p>The page you were looking for looking for doesn't exist.</p>
+    <span css={button} onClick={() => navigate("/")}>Return to Home</span>
+    </div>
+  </div>
 )
 
 export default NotFoundPage
+
+
+const fourError = css`
+  height: 100vh !important;
+  color: #fff;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  img{
+    max-width: 100%;
+    max-height: 50vh;
+  }
+  .text{
+    padding: 10px 50px 70px;
+  }
+`
