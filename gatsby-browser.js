451da310cb5311401eadcbd5cfe9bc9aba6dd4d9
Praveen G <praveenasvenom@gmail.com>
2019-07-24 16:23:54 +0530

    goback added for feedback

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,9 @@
  */
 
 // You can delete this file if you're not using it
+
+exports.onRouteUpdate = () => {
+  window.locations = window.locations || [document.referrer]
+  locations.push(window.location.href)
+  window.previousPath = locations[locations.length - 2]
+}
