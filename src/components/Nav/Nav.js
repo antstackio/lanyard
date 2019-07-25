15447a264d01fbbe47727f889bbf7c26dbdb04cb
Praveen G <praveenasvenom@gmail.com>
2019-07-25 20:50:33 +0530

    bug fixed and condition for feed back in nav menu

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,8 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 import { Link, navigate, useStaticQuery, graphql } from "gatsby"
 import { css } from "@emotion/core"
 import { media } from "../jss/cvcss"
+import _ from "lodash"
 
 import menu from "../../images/menu.svg"
 import logo from "../../images/logo.svg"
@@ -9,7 +10,19 @@ import agenda from "../../images/agenda.svg"
 import FlyMenu from "../FlyMenu/FlyMenu"
 import Variables from "../jss/Variables"
 
+
+
 const Nav = () => {
+  const [lsFeedBack, setLsFeedBack] = useState("")
+  const [lsSlots, setLsSlots] = useState("")
+
+  useEffect(() => {
+    setLsFeedBack(JSON.parse(localStorage.getItem("feedback")))
+    setLsSlots(JSON.parse(localStorage.getItem("slots")))
+  }, [])
+
+
+
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -19,6 +32,7 @@ const Nav = () => {
       }
     }
   `)
+
 
   const siteTitle = data.site.siteMetadata.title
 
@@ -44,10 +58,20 @@ const Nav = () => {
           <li className="contact ico large">
             <Link to="/Sponsers">Sponsers</Link>
           </li>
+        {lsSlots && !lsFeedBack.eventFeedback && (_.now() > lsSlots[lsSlots.length-1].timeEnd ) ?
+          ( <li className="contact ico large">
+            <Link to="/Feedback"
+                  state={
+                    {
+                      track: {
+                          title: "Event Feedback",
+                          trackId :  "eventFeedback"
+                        },
+                      overAllFeedback : true
+                      }
+                    }>Feedback</Link>
+          </li>): null}
 
-          <li className="contact ico large">
-            <Link to="/Feedback" state={{ track: { title: "Event Feedback", trackId :  "eventFeedback" } }}>Feedback</Link>
-          </li>
           {/* <hr></hr>
           <li className="about ico large">
             <Link to="/AboutLanyard">About Lanyard</Link>
